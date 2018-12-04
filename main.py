4d917d542c1855bd21897fe0bdd4dbff015e65b3
Ольга Шишкина <govorkova.t@yandex.ru>
2018-12-04 17:24:42 +0300

    delete all

--- main.py
+++ main.py
@@ -1,20 +1,2 @@
 from django import forms
 
-
-print("try to do something")
-
-print ("new change")
-
-print ("new new")
-
-print ("hey")
-
-print("message to  Masha")
-
-print("message to Lena")
-
-print("massage for all")
-
-print ("success")
-
-print ("not success")
