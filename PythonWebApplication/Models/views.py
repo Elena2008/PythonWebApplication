7f051fb15fc5f80e2b2c2c4e5e768259900c675a
Ольга Шишкина <govorkova.t@yandex.ru>
2018-12-26 23:57:24 +0300

    сделала форму регистрации

--- PythonWebApplication/Models/views.py
+++ PythonWebApplication/Models/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse_lazy
+from django.views import generic
+
+from .forms import PersonCreationForm
+
+class SignUp(generic.CreateView):
+    form_class = PersonCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'signup.html'
